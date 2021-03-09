3c4415ca8c36a0de313418e035d6cfe25473f4fc
Denis Korzun <dzianiskor@gmail.com>
2021-03-09 21:53:09 +0300

    feat: add DB connection

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
 const express = require('express')
 const config = require('config')
+const mongoose = require('mongoose')
 
 const app = express()
 const PORT = process.env.PORT || config.get('port') || 5000
 
-app.listen(PORT, () => {
-    console.log(`Server has been started on ${PORT} port`)
-})
+const start = async () => {
+    try {
+        await mongoose.connect(config.get('dataBaseUri'), {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true
+        })
+        app.listen(PORT, () => {
+            console.log(`Server has been started on ${PORT} port`)
+        })
+    } catch (e) {
+        console.log('Server error', e.message)
+        process.exit(1)
+    }
+}
+
+start()
+
+
